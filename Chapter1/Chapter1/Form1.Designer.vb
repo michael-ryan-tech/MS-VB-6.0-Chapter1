41c6d4adcf09bb97f843fe3a682515eb25c19150
Michael Ryan <michael.ryan.tech@gmail.com>
2021-01-05 21:36:32 -0800

    Completed form design and code. Added error handling.

--- Chapter1/Chapter1/Form1.Designer.vb
+++ Chapter1/Chapter1/Form1.Designer.vb
@@ -22,28 +22,127 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtWidth = New System.Windows.Forms.TextBox()
+        Me.txtArea = New System.Windows.Forms.TextBox()
+        Me.txtPerimeter = New System.Windows.Forms.TextBox()
+        Me.txtHeight = New System.Windows.Forms.TextBox()
+        Me.lblWidth = New System.Windows.Forms.Label()
+        Me.lblPerimeter = New System.Windows.Forms.Label()
+        Me.lblHeight = New System.Windows.Forms.Label()
+        Me.lblArea = New System.Windows.Forms.Label()
+        Me.cmdEvaluate = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'TextBox1
+        'txtWidth
         '
-        Me.TextBox1.Location = New System.Drawing.Point(22, 61)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 23)
-        Me.TextBox1.TabIndex = 0
+        Me.txtWidth.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtWidth.Location = New System.Drawing.Point(22, 61)
+        Me.txtWidth.Name = "txtWidth"
+        Me.txtWidth.Size = New System.Drawing.Size(158, 26)
+        Me.txtWidth.TabIndex = 1
+        '
+        'txtArea
+        '
+        Me.txtArea.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtArea.Location = New System.Drawing.Point(296, 112)
+        Me.txtArea.Name = "txtArea"
+        Me.txtArea.Size = New System.Drawing.Size(158, 26)
+        Me.txtArea.TabIndex = 8
+        '
+        'txtPerimeter
+        '
+        Me.txtPerimeter.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtPerimeter.Location = New System.Drawing.Point(296, 61)
+        Me.txtPerimeter.Name = "txtPerimeter"
+        Me.txtPerimeter.Size = New System.Drawing.Size(158, 26)
+        Me.txtPerimeter.TabIndex = 6
+        '
+        'txtHeight
+        '
+        Me.txtHeight.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtHeight.Location = New System.Drawing.Point(22, 112)
+        Me.txtHeight.Name = "txtHeight"
+        Me.txtHeight.Size = New System.Drawing.Size(158, 26)
+        Me.txtHeight.TabIndex = 3
+        '
+        'lblWidth
+        '
+        Me.lblWidth.AutoSize = True
+        Me.lblWidth.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblWidth.Location = New System.Drawing.Point(22, 40)
+        Me.lblWidth.Name = "lblWidth"
+        Me.lblWidth.Size = New System.Drawing.Size(45, 18)
+        Me.lblWidth.TabIndex = 0
+        Me.lblWidth.Text = "&Width"
+        '
+        'lblPerimeter
+        '
+        Me.lblPerimeter.AutoSize = True
+        Me.lblPerimeter.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblPerimeter.Location = New System.Drawing.Point(296, 40)
+        Me.lblPerimeter.Name = "lblPerimeter"
+        Me.lblPerimeter.Size = New System.Drawing.Size(70, 18)
+        Me.lblPerimeter.TabIndex = 5
+        Me.lblPerimeter.Text = "&Perimeter"
+        '
+        'lblHeight
+        '
+        Me.lblHeight.AutoSize = True
+        Me.lblHeight.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblHeight.Location = New System.Drawing.Point(22, 94)
+        Me.lblHeight.Name = "lblHeight"
+        Me.lblHeight.Size = New System.Drawing.Size(49, 18)
+        Me.lblHeight.TabIndex = 2
+        Me.lblHeight.Text = "&Height"
+        '
+        'lblArea
+        '
+        Me.lblArea.AutoSize = True
+        Me.lblArea.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblArea.Location = New System.Drawing.Point(296, 94)
+        Me.lblArea.Name = "lblArea"
+        Me.lblArea.Size = New System.Drawing.Size(38, 18)
+        Me.lblArea.TabIndex = 7
+        Me.lblArea.Text = "&Area"
+        '
+        'cmdEvaluate
+        '
+        Me.cmdEvaluate.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.cmdEvaluate.Location = New System.Drawing.Point(202, 40)
+        Me.cmdEvaluate.Name = "cmdEvaluate"
+        Me.cmdEvaluate.Size = New System.Drawing.Size(75, 30)
+        Me.cmdEvaluate.TabIndex = 4
+        Me.cmdEvaluate.Text = "&Evaluate"
+        Me.cmdEvaluate.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.TextBox1)
+        Me.ClientSize = New System.Drawing.Size(473, 176)
+        Me.Controls.Add(Me.cmdEvaluate)
+        Me.Controls.Add(Me.lblArea)
+        Me.Controls.Add(Me.lblHeight)
+        Me.Controls.Add(Me.lblPerimeter)
+        Me.Controls.Add(Me.lblWidth)
+        Me.Controls.Add(Me.txtHeight)
+        Me.Controls.Add(Me.txtPerimeter)
+        Me.Controls.Add(Me.txtArea)
+        Me.Controls.Add(Me.txtWidth)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Rectangle Demo"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents txtWidth As TextBox
+    Friend WithEvents txtArea As TextBox
+    Friend WithEvents txtPerimeter As TextBox
+    Friend WithEvents txtHeight As TextBox
+    Friend WithEvents lblWidth As Label
+    Friend WithEvents lblPerimeter As Label
+    Friend WithEvents lblHeight As Label
+    Friend WithEvents lblArea As Label
+    Friend WithEvents cmdEvaluate As Button
 End Class
--- Chapter1/Chapter1/Form1.vb
+++ Chapter1/Chapter1/Form1.vb
@@ -2,4 +2,52 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub cmdEvaluate_Click(sender As Object, e As EventArgs) Handles cmdEvaluate.Click
+
+        Dim dblWidth As Double, dblHeight As Double
+        Dim strWidth As String, strHeight As String
+        Dim blIsNumber As Boolean
+
+        blIsNumber = False
+
+        strWidth = ""
+        strHeight = ""
+
+        If txtWidth.Text = "" Then
+
+            MsgBox("Please enter width", MsgBoxStyle.Exclamation + MsgBoxStyle.OkOnly, "Missing Width")
+
+            Exit Sub
+
+        End If
+
+        If txtHeight.Text = "" Then
+
+            MsgBox("Please enter height", MsgBoxStyle.Exclamation + MsgBoxStyle.OkOnly, "Missing height")
+
+            Exit Sub
+
+        End If
+
+        strWidth = txtWidth.Text
+        strHeight = txtHeight.Text
+
+        blIsNumber = IsNumeric(strWidth) And IsNumeric(strHeight)
+
+        If blIsNumber = False Then
+
+            MsgBox("Please Enter a Number", MsgBoxStyle.OkOnly, "Numbers Only")
+
+            Exit Sub
+
+        End If
+
+        dblWidth = CDbl(strWidth)
+        dblHeight = CDbl(strHeight)
+
+        txtPerimeter.Text = CStr((dblWidth + dblHeight) * 2)
+        txtArea.Text = CStr(dblWidth * dblHeight)
+
+    End Sub
 End Class
